cfe68ceab9b7a3e1e9673ee94016453932172b76
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-07-06 12:47:09 -0500

    made aces count as 1 and 11.

--- blackjack.py
+++ blackjack.py
@@ -1,26 +1,73 @@
 from random import randint, shuffle
 
 
+def card_score(card):
+    if isinstance(card, int):
+        return card
+    elif card == 'J':
+        card = 10
+    elif card == 'K':
+        card = 10
+    elif card == 'Q':
+        card = 10
+    elif card == 'A':
+        card = 11
+    return card
+
+
+def hand_score(hand):
+    total = 0
+    for card in hand:
+        total += card_score(card)
+    return total
+
+
+def hand_scores(hand):
+    '''
+    >>> hand_scores([2,3,4])
+    [9]
+    >>> hand_scores(['Q', 2, 5])
+    [17]
+    >>> hand_scores(['A', 5])
+    [6, 16]
+    >>> hand_scores(['A', 5, 'A'])
+    [7, 17, 27]
+    '''
+    interpretations = []
+    hand_card_scores = []
+
+    if not hand:
+        return [[]]
+    elif hand[0] == 'A':
+        sub_interps = interpretations(hand[1:])
+        as_one = [[1] + sub for sub in sub_interps]
+        as_eleven = [[11] + sub for sub in sub_interps]
+        return as_one + as_eleven
+    else:
+        sub_interps = interpretations(hand[1:])
+        as_self = [card_score[hand[0]] + sub for sub in sub_interps]
+        return as_self
+
+
 def blackjack():
+
     deck = [
         2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7,
-        8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-        10, 10, 10, 10, 11, 11, 11, 11
+        8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 'Q', 'Q', 'Q', 'Q', 'K', 'K',
+        'K', 'K', 'J', 'J', 'J', 'J', 'A', 'A', 'A', 'A'
     ]
+
     shuffle(deck)
     blackjack_hand = []
     dealer_hand = []
-    count = 0
-
     for _ in range(2):
         blackjack_hand.append(deck.pop())
         dealer_hand.append(deck.pop())
+    while hand_score(blackjack_hand) < 22:
 
-    while sum(blackjack_hand) < 22:
-        count += 1
-        print('{} player: {}, dealer: {}'.format(count, sum(blackjack_hand),
-                                                 dealer_hand[0]))
-        if sum(blackjack_hand) == 21:
+        print('player: {}, dealer: {}'.format(
+            hand_score(blackjack_hand), dealer_hand[0]))
+        if hand_score(blackjack_hand) == 21:
             break
         option = input('Would you like to hit or stay? enter "h" to hit. ')
         if option == 'h':
@@ -28,30 +75,32 @@ def blackjack():
             continue
         else:
             break
+    print(blackjack_hand, dealer_hand[0])
 
-    while sum(blackjack_hand) < 22 and sum(dealer_hand) < 22:
-        if sum(dealer_hand) < 17:
+    while hand_score(blackjack_hand) < 22 and hand_score(dealer_hand) < 22:
+        if hand_score(dealer_hand) <= 17:
             dealer_hand.append(deck.pop())
             print('dealer hits!')
             input('press any key to continue')
-        elif sum(dealer_hand) >= 17:
+        elif hand_score(dealer_hand) >= 17:
             print('dealer stays')
             break
 
-    print('{} player: {}, dealer: {}'.format(count, sum(blackjack_hand),
-                                             sum(dealer_hand)))
+    print('player: {}, dealer: {}'.format(
+        hand_score(blackjack_hand), hand_score(dealer_hand)))
+    print(blackjack_hand, dealer_hand)
 
-    if sum(blackjack_hand) > 21 and sum(dealer_hand) <= 21:
+    if hand_score(blackjack_hand) > 21 and hand_score(dealer_hand) < 21:
         print('DEALER WINS')
-    elif sum(dealer_hand) > 21 and sum(blackjack_hand) <= 21:
+    elif hand_score(dealer_hand) > 21 and hand_score(blackjack_hand) < 21:
         print('YOU WIN')
-    elif sum(dealer_hand) > 21 and sum(blackjack_hand) > 21:
+    elif hand_score(dealer_hand) > 21 and hand_score(blackjack_hand) > 21:
         print('BUST')
-    elif sum(dealer_hand) == sum(blackjack_hand):
+    elif hand_score(dealer_hand) == hand_score(blackjack_hand):
         print('PUSH')
-    elif (sum(blackjack_hand) > sum(dealer_hand)):
+    elif (hand_score(blackjack_hand) > hand_score(dealer_hand)):
         print('YOU WIN')
-    elif sum(blackjack_hand) < sum(dealer_hand):
+    elif hand_score(blackjack_hand) < hand_score(dealer_hand):
         print('DEALER WINS')
 
 
